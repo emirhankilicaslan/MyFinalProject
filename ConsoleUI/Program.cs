7cd77ad41e742496b44e3d46e9413d5d688f066c
Emirhan Kilicaslan <emirhannkilicaslann@gmail.com>
2022-10-27 15:15:35 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,22 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using System;
+
+namespace ConsoleUI
+{
+    //SOLID 
+    // Open closed principle
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            foreach (var product in productManager.GetAllByCategoryId(2))
+            {
+                Console.WriteLine(product.ProductName);
+            }
+            
+        }
+    }
+}
